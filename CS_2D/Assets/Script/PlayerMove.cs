10e46f771cf0b421f04397fb92f4f8d18d8792aa
JiwonLee <85108663+JiwonLee-2@users.noreply.github.com>
2023-07-07 00:20:03 +0900

    캐릭터 생성 및 이동구현 공부

---
+++ CS_2D/Assets/Script/PlayerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float h;
+    public float v;
+
+    void Start()
+    {
+        h = 0;
+        v = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = Input.GetAxisRaw("Vertical");
+        h = Input.GetAxisRaw("Horizontal");
+        Vector3 vec = new Vector3(speed * Time.deltaTime * h, speed * Time.deltaTime * v, 0); //이동속도 제한
+        transform.Translate(vec);
+
+
+    }
+}
