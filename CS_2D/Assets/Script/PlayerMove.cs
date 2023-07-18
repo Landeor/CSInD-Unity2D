0a67b03b422b53c996bf627e03e3a7f5b6680160
JiwonLee <85108663+JiwonLee-2@users.noreply.github.com>
2023-07-18 18:22:59 +0900

    플레이어 이동 및 이동 애니메이션
    오류가 나욥,,,

--- CS_2D/Assets/Script/PlayerMove.cs
+++ CS_2D/Assets/Script/PlayerMove.cs
@@ -1,25 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMove : MonoBehaviour
 {
     public float speed;
-    public float h;
-    public float v;
-
-    void Start()
+    float h;
+    float v;
+    bool isHorizonMove;
+    
+    spriteRenderer spriter;
+    Rigidbody2D rigid;
+    
+    void Awake()
     {
-        h = 0;
-        v = 0;
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
     }
-
     void Update()
     {
-        v = Input.GetAxisRaw("Vertical");
+        //Move Value
         h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
 
-        Vector3 vec = new Vector3(speed * Time.deltaTime * h, speed * Time.deltaTime * v, 0); //이동속도 제한
-        transform.Translate(vec);
+        //Check Vutton Down & Up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        //Check Horizontal Move
+        if(hDown || vUp){
+            isHorizonMove = true;
+        }
+        else if(vDown || hUp){
+            isHorizonMove = false;
+        }
+        //Animation
+        anim.SetInteger("hAxisRaw", (int)h);
+        anim.SetInteger("vAxisRaw", (int)v);
+    }
+    void FixedUpdate() {
+        //위치 이동
+        Vector2 moveVec = isHorizonMove ? new Vector2(h,0) : new Vector2(0,v);
+        rigid.velocity = moveVec * speed;
+    }
+    void LateUpdate(){
+        if (h != 0){
+            spriter.flipX = h < 0; 
+        }
     }
 }
